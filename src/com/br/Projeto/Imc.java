9b9c04b265686bc0c95d8f75073d1ad57e062263
luizcarlosslj <luizcarlosslj@gmail.com>
2015-11-10 21:36:24 -0200

    luiz carlos
    finalizado

--- src/com/br/Projeto/Imc.java
+++ src/com/br/Projeto/Imc.java
@@ -9,10 +9,14 @@ public class Imc {
 		double peso;
 		double altura;
 		double imc;
+		String nome;
+		
 		System.out.println("escreva seu peso");
 		peso = dados.nextDouble();
 		System.out.println("escreva sua altura");
 		altura = dados.nextDouble();
+		System.out.println("escreva seu nome");
+		nome = dados.next();
 		
 		imc = peso / (altura * altura);
 		
@@ -20,15 +24,20 @@ public class Imc {
 			System.out.println("*Voce está abaixo do peso ideal");
 		}
 		if(imc >=18.5 && imc <=24.9){
-					System.out.println("Parabens - voce esta normal nao ");
-				}
+			System.out.println("Parabens - voce esta normal nao ");
+		}
 		if(imc >=25 && imc <= 29.9){
 			System.out.println("Voce ta meio gorda")
 		}
-			}
+		if (imc > 30.0){
+			System.out.printn("Voce ta gordão bixão")
 		}
+
+	}
+}
 		
-		System.out.println("O imc eh " + imc);
+		
+		System.out.println(nome + ", o seu imc eh: " + imc);
 		
 		
 		
