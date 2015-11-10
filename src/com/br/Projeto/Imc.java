b96aabe17df905d4069489464da445c4fb3e4e5e
rodrigosk <rodrigo@actuar.com>
2015-11-10 21:28:29 -0200

    Feito 3 testes com IMC
    Foi feito 3 testes com o IMC

--- src/com/br/Projeto/Imc.java
+++ src/com/br/Projeto/Imc.java
@@ -16,6 +16,18 @@ public class Imc {
 		
 		imc = peso / (altura * altura);
 		
+		if(imc < 18.5){
+			System.out.println("*Voce está abaixo do peso ideal");
+		}
+		if(imc >=18.5 && imc <=24.9){
+					System.out.println("Parabens - voce esta normal nao ");
+				}
+		if(imc >=25 && imc <= 29.9){
+			System.out.println("Voce ta meio gorda")
+		}
+			}
+		}
+		
 		System.out.println("O imc eh " + imc);
 		
 		
